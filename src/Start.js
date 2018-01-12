d23fec97e9cac334d991ae9cfecb00f980fb978b
Jared Griffin <griffinjared1388@gmail.com>
2018-01-12 11:46:23 -0600

    Created separate inputs for each player's name

--- src/Start.js
+++ src/Start.js
@@ -6,6 +6,27 @@ const styles = {
         marginTop: '20rem',
         fontSize: '3rem',
         fontWeight: 'normal'
+    },
+    bluePlayerContainer: {
+        backgroundColor: '#73B1E6',
+        display: 'inline-block',
+        width: '50rem',
+        marginLeft: '10rem',
+        marginTop: '10rem'
+    },
+    bluePlayerInputLabel: {
+        color: 'white'
+    },
+    redPlayerContainer: {
+        backgroundColor: '#CE6E6E',
+        display: 'inline-block',
+        width: '50rem',
+        float: 'right',
+        marginRight: '10rem',
+        marginTop: '10rem'
+    },
+    redPlayerInputLabel: {
+        color: '#111111'
     }
 }
 
@@ -14,7 +35,14 @@ export default class Start extends Component {
         return (
             <div>
                 <h1 style={styles.startHeader}>Players, enter your names below!</h1>
-                
+                <div style={styles.bluePlayerContainer}>
+                    <div style={styles.bluePlayerInputLabel}>Blue Player's Name</div>
+                    <input/>
+                </div>
+                <div style={styles.redPlayerContainer}>
+                    <div style={styles.redPlayerInputLabel}>Red Player's Name</div>
+                    <input/>
+                </div>
             </div>
         );
     }
