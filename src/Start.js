1b03e9247599de36423879fe747fb1bf38d1f79b
Jared Griffin <griffinjared1388@gmail.com>
2018-01-16 11:50:20 -0600

    Styled player name inputs

--- src/Start.js
+++ src/Start.js
@@ -5,17 +5,33 @@ const styles = {
         textAlign: 'center',
         marginTop: '20rem',
         fontSize: '3rem',
-        fontWeight: 'normal'
+        fontWeight: 'normal',
+        color: ''
     },
     bluePlayerContainer: {
         backgroundColor: '#73B1E6',
         display: 'inline-block',
         width: '50rem',
         marginLeft: '10rem',
-        marginTop: '10rem'
+        marginTop: '10rem',
+        textAlign: 'center',
+        paddingTop: '1rem',
+        paddingBottom: '1.5rem',
+        textAlign: 'center',
+        borderRadius: '1.5rem'
     },
     bluePlayerInputLabel: {
-        color: 'white'
+        color: 'white',
+        fontSize: '2rem',
+        paddingBottom: '1rem'
+    },
+    bluePlayerInput: {
+        borderRadius: '0.5rem',
+        borderColor: '#1F1FAB',
+        borderStyle: 'solid',
+        width: '50%',
+        padding: '0.5rem',
+        fontSize: '1rem'
     },
     redPlayerContainer: {
         backgroundColor: '#CE6E6E',
@@ -23,10 +39,24 @@ const styles = {
         width: '50rem',
         float: 'right',
         marginRight: '10rem',
-        marginTop: '10rem'
+        marginTop: '10rem',
+        paddingTop: '1rem',
+        paddingBottom: '1.5rem',
+        textAlign: 'center',
+        borderRadius: '1.5rem'
     },
     redPlayerInputLabel: {
-        color: '#111111'
+        color: 'white',
+        fontSize: '2rem',
+        paddingBottom: '1rem'
+    },
+    redPlayerInput: {
+        borderRadius: '0.5rem',
+        borderColor: '#AD0E2A',
+        borderStyle: 'solid',
+        width: '50%',
+        padding: '0.5rem',
+        fontSize: '1rem'
     }
 }
 
@@ -36,12 +66,12 @@ export default class Start extends Component {
             <div>
                 <h1 style={styles.startHeader}>Players, enter your names below!</h1>
                 <div style={styles.bluePlayerContainer}>
-                    <div style={styles.bluePlayerInputLabel}>Blue Player's Name</div>
-                    <input/>
+                    <div style={styles.bluePlayerInputLabel}>Blue Player</div>
+                    <input style={styles.bluePlayerInput}/>
                 </div>
                 <div style={styles.redPlayerContainer}>
-                    <div style={styles.redPlayerInputLabel}>Red Player's Name</div>
-                    <input/>
+                    <div style={styles.redPlayerInputLabel}>Red Player</div>
+                    <input style={styles.redPlayerInput}/>
                 </div>
             </div>
         );
