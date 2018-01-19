728d2099c621b7a585efa8def0da9bb03cb25ca1
Jared Griffin <griffinjared1388@gmail.com>
2018-01-19 11:24:53 -0600

    Added missing import

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import BoardRow from './helpers/components/BoardRow';
+import Actions from './reducers/Actions';
 
 import Styles from './styles/TicTacToe';
 
